04caa6f0928c51188a92c6b8189f7e3d347b335f
armando <armando@AMD>
2024-05-09 16:41:50 -0500

    Ajustes terminados

--- src/main/java/main/ViewLogin.java
+++ src/main/java/main/ViewLogin.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package main;
 
-/**
- *
- * @author armando
- */
+
+// @author armando
+
 public class ViewLogin extends javax.swing.JFrame {
 
    
